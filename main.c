7d468e0e3083a699736f454523e4a0f34900a024
Adriana <adriana.lohz@gmail.com>
2015-04-28 19:16:00 -0500

    Se empezó a hacer la interfaz de línea de comando. :

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 int main(int argc, char const *argv[])
 {
 	/* code */
-	printf("Hola\n");
+	
+	printf("%s\n", "==================================================");
+	printf("||\tHola Bienvenido A Tu Base De Datos\t||\n");
+	printf("||\t\t\t\t\t\t||\n");
+	printf("||º t - Lista todas las canciones por título \t||\n");
+	printf("||º a - Lista todas las canciones por año\t||\n");
+	printf("||º d - Lista todas las canciones por duración\t||\n");
+	printf("||º g - Lista todas las canciones por género\t||\n");
+	printf("||º i - Lista todas las canciones por intérprete||\n");	
+	printf("%s\n", "==================================================");
 	return 0;
 }
