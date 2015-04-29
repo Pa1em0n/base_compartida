e7d8d6b7a295755c5b843d325c46a740619f5180
lpmv <pauljoystick@gmail.com>
2015-04-28 19:41:04 -0500

    se implementaron funciones indicadoras

--- busqueda.c
+++ busqueda.c
@@ -1,7 +1,8 @@
 #include "estructuras.h"
+#include <stdlib.h>
 
 int
-indicadora(cancion * can, char * nombre)
+indicadora1(cancion * can, char * nombre)
 {
 	if (nombre == can->nombre)
 		return 1;
@@ -9,4 +10,35 @@ indicadora(cancion * can, char * nombre)
 		return 0;
 }
 
+int
+indicadora2(cancion * can, char * interprete)
+{
+    if(interprete == can->interprete)
+        return 1;
+    else
+        return 0;
+}
 
+int indicadora3(cancion * can, char * genero)
+{
+    if(genero == can->genero)
+        return 1;
+    else
+        return 0;
+}
+
+nodo * 
+busqueda(nodo * lista, int (*funcion) ())
+{
+    nodo * list = (nodo*) malloc(sizeof(nodo));
+    while(lista != NULL)
+    {
+           
+    }
+}   
+
+void
+agregaLista(nodo * lista, char * dato)
+{
+    
+}
